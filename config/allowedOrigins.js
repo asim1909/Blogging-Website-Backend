a419502870915e8e8bb0502f70e5e91d2be1242f
出久 <118390636+asim1909@users.noreply.github.com>
2024-07-26 01:29:31 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://reliable-semifreddo-e358df.netlify.app'
+    'https://main--bloggco.netlify.app/'
 
 ];
 
